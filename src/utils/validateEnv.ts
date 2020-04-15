3882784da6f7f78ef45381b2394fa25f3ad9beac
Frederick Johnson <fritzj96@gmail.com>
2020-04-14 22:59:44 -0700

    added: node_env to envalid

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -2,6 +2,7 @@ import { cleanEnv, port, str } from 'envalid';
 
 export function validateEnv() {
   cleanEnv(process.env, {
+    NODE_ENV: str(),
     JWT_SECRET: str(),
     POSTGRES_HOST: str(),
     POSTGRES_PORT: port(),
