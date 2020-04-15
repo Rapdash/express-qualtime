b36eaf265bcfdc8b5fc0f2d72f3898682b3bedbd
Frederick Johnson <fritzj96@gmail.com>
2020-04-14 23:48:03 -0700

    added: createJwt

---
+++ src/auth/utils/createJwt.ts
@@ -0,0 +1,11 @@
+import { UserEntity } from "../../user";
+import * as jwt from "jsonwebtoken";
+
+export const createToken = (user: UserEntity): string => {
+  const expiresIn = 60 * 60 * 24; // One Day
+  const secret = process.env.JWT_SECRET;
+  const dataStoredInToken: { id: string } = {
+    id: user.id,
+  };
+  return jwt.sign(dataStoredInToken, secret, { expiresIn });
+};
