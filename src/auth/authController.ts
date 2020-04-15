e0fbf9d1e2709939fe6c397b986cee0389d66f13
Frederick Johnson <fritzj96@gmail.com>
2020-04-14 23:49:12 -0700

    working on createJwt function

--- src/auth/authController.ts
+++ src/auth/authController.ts
@@ -1,7 +1,30 @@
 import * as bcrypt from "bcrypt";
-import * as express from "express";
+import { Request, Response, NextFunction} from "express";
 import * as jwt from "jsonwebtoken";
 import { getRepository } from "typeorm";
 
-import { } from "../errors";
+import { EmailTakenError } from "../errors";
 import { UserEntity } from "../user";
+import { RegisterUserDto } from "./RegisterUserDto";
+import { createJwt } from "./utils/createJwt";
+
+export const register = async (request: Request, response: Response, next: NextFunction) => {c
+  const userRepository = getRepository(UserEntity);
+  const newUserData: RegisterUserDto = request.body;
+  // if this throws, the router-level async handler will catch it.
+  // I prefer to handle "unexpected" errors like this could be with a library.
+  if (await userRepository.findOne({ email: newUserData.email })) {
+    // This error, on the other hand, is handled explicitly with next().
+    // I prefer to follow the express pattern when intentionally throwing.
+    next(new EmailTakenError(newUserData.email));
+  } else {
+    const password_hash = await bcrypt.hash(newUserData.password, 10);
+    const user = userRepository.create({
+      ...newUserData, 
+      password_hash
+    });
+    await userRepository.save(user);
+    user.password_hash = undefined;
+    const tokenData = createJwt(user);
+  }
+}
--- src/auth/utils/createJwt.ts
+++ src/auth/utils/createJwt.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from "../../user";
 import * as jwt from "jsonwebtoken";
 
-export const createToken = (user: UserEntity): string => {
+export const createJwt = (user: UserEntity): string => {
   const expiresIn = 60 * 60 * 24; // One Day
   const secret = process.env.JWT_SECRET;
   const dataStoredInToken: { id: string } = {
