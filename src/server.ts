fdf39272ffaed6e08bb3c56b9a861af85b6f878f
Frederick Johnson <fritzj96@gmail.com>
2020-04-14 22:51:26 -0700

    added: basic server with no routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { config as configureEnv } from "dotenv";
 import { createConnection } from "typeorm"
 import express, { json } from "express";
+import logger from "morgan";
 
 import { validateEnv, ormConfig } from "./utils";
 
@@ -19,6 +20,12 @@ const main = async () => {
   const app = express();
   // Set up JSON parsing from req.body
   app.use(json());
-  // Add 
-  app.
+  // Add logging via morgan
+  app.use(logger("dev"));
+
+  app.listen(process.env.PORT || 1337, () => {
+    console.info(`Server Started on ${process.env.PORT}`);
+  });
 }
+
+main();
